bd8c0e8c347f05a09275e914546ca6a6203f4e84
JuanCarlosLP <jclarios.js@gmail.com>
2021-01-05 21:47:21 -0600

    Node Server

--- index.js
+++ index.js
@@ -3,59 +3,37 @@ let fs = require("fs");
 
 http.createServer((request, response) => {
     response.setHeader("Content-Type", "text/html; charset=utf-8");
-    if (request.url === "/contacto") {
-      fs.readFile("./contact.html", (error, content) => {
-        if(!error){
+
+    const readFile = (page, response) => {
+      fs.readFile(page, (error, content) => {
+         if(!error){
             response.write(content);
             response.end();
-        }else{
-            response.write("<h1>404</h1>");
+         }else{
+            response.write(notFound);
             response.end();
-        }
+         }
       });
-    } else if (request.url === "/proyectos") {
-      fs.readFile("./projects.html", (error, content) => {
-        if(!error){
-            response.write(content);
-            response.end();
-        }else{
-            response.write("<h1>404</h1>");
-            response.end();
-        }
-      });
-    } else if (request.url === "/nosotros") {
-      fs.readFile("./about.html", (error, content) => {
-        if(!error){
-            response.write(content);
-            response.end();
-        }else{
-            response.write("<h1>404</h1>");
-            response.end();
-        }
-      });
-    } else if (request.url === "/") {
-      fs.readFile("./index.html", (error, content) => {
-        if(!error){
-            response.write(content);
-            response.end();
-        }else{
-            response.write("<h1>404</h1>");
-            response.end();
-        }
-      });
-    } else if (request.url === "/favicon.ico") {
-      fs.readFile("./favicon.ico", (error, content) => {
-        if(!error){
-            response.write(content);
-            response.end();
-        }else{
-            response.write("<h1>404</h1>");
-            response.end();
-        }
-      });
-    }
-    else {
-      response.write("<h1>404</h1>");
-      response.end();
+   };
+
+    switch(request.url) {
+      case "/":
+        readFile("./index.html" , response);
+        break;
+      case "/nosotros":
+        readFile("./about.html" , response);
+        break;
+      case "/proyectos":
+        readFile("./projects.html" , response);
+        break;
+      case "/contacto":
+        readFile("./contact.html" , response);
+        break;
+      case "/favicon.ico":
+        readFile("./favicon.ico" , response);
+        break;
+      default:
+        readFile("./404.html" , response);
+        break;
     }
 }).listen(8080);
