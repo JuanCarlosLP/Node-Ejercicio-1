256cd7ab428599aa78268057e47017f4b3f83859
JuanCarlosLP <jclarios.js@gmail.com>
2021-01-05 20:50:24 -0600

    Primer Servidor Node

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let fs = require("fs");
 http.createServer((request, response) => {
     response.setHeader("Content-Type", "text/html; charset=utf-8");
     if (request.url === "/contacto") {
-      fs.readFile("./contacto.html", (error, content) => {
+      fs.readFile("./contact.html", (error, content) => {
         if(!error){
             response.write(content);
             response.end();
@@ -13,18 +13,48 @@ http.createServer((request, response) => {
             response.end();
         }
       });
-    } else if (request.url === "/") {     
-      response.write("<h1>Página de inicio</h1>");
-      response.end();
-    } else if (request.url === "/usuarios") {  
-        response.setHeader("Content-Type", "application/json; charset=utf-8");   
-        let users = [{
-            nombre: "Javier",
-            email: "javier09@gmail.com"
-        }]
-        response.write(JSON.stringify(users));
-        response.end();
-    } else {
+    } else if (request.url === "/proyectos") {
+      fs.readFile("./projects.html", (error, content) => {
+        if(!error){
+            response.write(content);
+            response.end();
+        }else{
+            response.write("<h1>404</h1>");
+            response.end();
+        }
+      });
+    } else if (request.url === "/nosotros") {
+      fs.readFile("./about.html", (error, content) => {
+        if(!error){
+            response.write(content);
+            response.end();
+        }else{
+            response.write("<h1>404</h1>");
+            response.end();
+        }
+      });
+    } else if (request.url === "/") {
+      fs.readFile("./index.html", (error, content) => {
+        if(!error){
+            response.write(content);
+            response.end();
+        }else{
+            response.write("<h1>404</h1>");
+            response.end();
+        }
+      });
+    } else if (request.url === "/favicon.ico") {
+      fs.readFile("./favicon.ico", (error, content) => {
+        if(!error){
+            response.write(content);
+            response.end();
+        }else{
+            response.write("<h1>404</h1>");
+            response.end();
+        }
+      });
+    }
+    else {
       response.write("<h1>404</h1>");
       response.end();
     }
